1ede05acb4b435156a0b4188c91ba1acfed5d136
Frederickbeaud <fredbeaud.fb@gmail.com>
2022-09-16 04:41:00 +0100

    modification

--- src/main/java/tech/getarrays/employeemanager/EmployeeRessource.java
+++ src/main/java/tech/getarrays/employeemanager/EmployeeRessource.java
@@ -3,9 +3,7 @@ package tech.getarrays.employeemanager;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.getarrays.employeemanager.model.Employee;
 import tech.getarrays.employeemanager.service.EmployeeService;
 
@@ -20,9 +18,25 @@ public class EmployeeRessource {
         this.employeeService = employeeService;
     }
 
-    @GetMapping
+
+    @GetMapping("/all")
     public ResponseEntity<List<Employee>> getAllEmployees(){
         List<Employee> employees =employeeService.findAllEmployees();
         return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id){
+        Employee employee =employeeService.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+    @PostMapping("/add")
+    public  ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+    @PutMapping("/update")
+    public  ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
+        Employee updateEmployee = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
     }
 }
