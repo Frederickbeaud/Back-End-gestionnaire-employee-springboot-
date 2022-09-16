25fcaaf339b1015e1de77f36ef3616314a731995
Frederickbeaud <fredbeaud.fb@gmail.com>
2022-09-16 04:20:03 +0100

    modification

---
+++ src/main/java/tech/getarrays/employeemanager/EmployeeRessource.java
@@ -0,0 +1,28 @@
+package tech.getarrays.employeemanager;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.getarrays.employeemanager.model.Employee;
+import tech.getarrays.employeemanager.service.EmployeeService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeRessource {
+    private final EmployeeService employeeService;
+
+    public EmployeeRessource(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees(){
+        List<Employee> employees =employeeService.findAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+}
